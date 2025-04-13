94026633f1ae065f64c98ae0b8b54d6bad69f83c
Troy Sharples <troysharples@hotmail.co.uk>
2025-04-13 21:49:55 +0100

    Improved chess board printing with full ANSI forground and background colours

--- src/position/position.cpp
+++ src/position/position.cpp
@@ -6,16 +6,13 @@
 namespace 
 {
 
-constexpr const char* FG_RED        = "\033[31m";
-constexpr const char* FG_GREEN      = "\033[32m";
-constexpr const char* FG_BLUE       = "\033[34m";
-constexpr const char* FG_LIGHT_GREY = "\033[37m";
-constexpr const char* FG_DEFAULT    = "\033[39m";
-constexpr const char* FG_DARK_GREY  = "\033[49m";
-constexpr const char* BG_RED        = "\033[41m";
-constexpr const char* BG_GREEN      = "\033[42m";
-constexpr const char* BG_BLUE       = "\033[44m";
-constexpr const char* BG_DEFAULT    = "\033[49m";
+constexpr const char* ANSI_RESET = "\e[0m";
+
+constexpr const char* ANSI_BOLD_BLACK = "\e[1;30m";
+constexpr const char* ANSI_BOLD_WHITE = "\e[1;37m";
+
+constexpr const char* ANSI_BACKGROUND_BLACK = "\e[40m";
+constexpr const char* ANSI_BACKGROUND_WHITE = "\e[47m";
 
 enum piece
 {
@@ -30,29 +27,30 @@ std::ostream& operator<<(std::ostream& os, piece v)
 {
     switch (v)
     {
-        case white_pawn:   return os << FG_LIGHT_GREY << "P ";
-        case white_knight: return os << FG_LIGHT_GREY << "Kn";
-        case white_king:   return os << FG_LIGHT_GREY << "K ";
-        case white_rook:   return os << FG_LIGHT_GREY << "R ";
-        case white_queen:  return os << FG_LIGHT_GREY << "Q ";
-        case white_bishop: return os << FG_LIGHT_GREY << "B ";
-        case black_pawn:   return os << FG_DARK_GREY  << "P ";
-        case black_knight: return os << FG_DARK_GREY  << "Kn";
-        case black_king:   return os << FG_DARK_GREY  << "K ";
-        case black_rook:   return os << FG_DARK_GREY  << "R ";
-        case black_queen:  return os << FG_DARK_GREY  << "Q ";
-        case black_bishop: return os << FG_DARK_GREY  << "B ";
+        case white_pawn:   return os << ANSI_BOLD_WHITE << "P ";
+        case white_knight: return os << ANSI_BOLD_WHITE << "Kn";
+        case white_king:   return os << ANSI_BOLD_WHITE << "K ";
+        case white_rook:   return os << ANSI_BOLD_WHITE << "R ";
+        case white_queen:  return os << ANSI_BOLD_WHITE << "Q ";
+        case white_bishop: return os << ANSI_BOLD_WHITE << "B ";
+        case black_pawn:   return os << ANSI_BOLD_BLACK << "P ";
+        case black_knight: return os << ANSI_BOLD_BLACK << "Kn";
+        case black_king:   return os << ANSI_BOLD_BLACK << "K ";
+        case black_rook:   return os << ANSI_BOLD_BLACK << "R ";
+        case black_queen:  return os << ANSI_BOLD_BLACK << "Q ";
+        case black_bishop: return os << ANSI_BOLD_BLACK << "B ";
         default: return os << "  ";
     }
 }
 
 std::ostream& operator<<(std::ostream& os, const mailbox_board& v)
 {
+    os << ANSI_RESET;
     for (std::size_t rank = 0; rank < 8; rank++)
     {
         for (std::size_t file = 0; file < 8; file++)
-            os << v[(7-rank)*8+file] << ' ';
-        os << '\n';
+            os << (WHITE_SQUARES & get_bitboard_mailbox_piece(rank*8+file) ? ANSI_BACKGROUND_BLACK : ANSI_BACKGROUND_WHITE) << v[(7-rank)*8+file] << ' ';
+        os << ANSI_RESET << '\n';
     }
     return os;
 }
