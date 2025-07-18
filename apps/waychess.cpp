722446e6d0bf23c9ad2b9cea6a1f15662786861e
Troy Sharples <troysharples@hotmail.co.uk>
2025-07-18 17:21:17 +0100

    Added more test cases and improved layout for the current main

--- apps/waychess.cpp
+++ apps/waychess.cpp
@@ -9,6 +9,21 @@
 #include "position/mailbox.hpp"
 #include "position/generate_moves.hpp"
 #include "position/make_move.hpp"
+#include "position/move.hpp"
+
+static void display_pseudo_legal_moves_from_position(const bitboard& start)
+{
+    std::cout << "starting-position:" << std::endl;
+    std::cout << start << std::endl;
+
+    for (auto move : generate_pseudo_legal_moves(start))
+    {
+        std::cout << "move: " << move::to_algebraic_long(move) << ":" << std::endl;
+        bitboard next_position = start;
+        make_move(next_position, move);
+        std::cout << next_position << std::endl;
+    }
+}
 
 int main()
 {
@@ -142,35 +157,26 @@ int main()
     // }
     // std::cout << '\n';
 
-    constexpr const char* STARTING_FEN { "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" };
-    std::cout << "starting-position\n";
-    std::cout << bitboard(STARTING_FEN);
-    std::cout << '\n';
+    std::cout << "move-generation-testing:\n\n";
 
-    constexpr const char* COMPLICATED_FEN { "r3k2r/pp1n2pp/2p2q2/b2p1n2/BP1Pp3/P1N2P2/2PB2PP/R2Q1RK1 w kq b3 0 13" };
-    std::cout << "complicated-position\n";
-    std::cout << bitboard(COMPLICATED_FEN);
-    std::cout << '\n';
+    std::vector<std::string> positions {
+        // Starting position.
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        // Complicated middlegame.
+        "r3k2r/pp1n2pp/2p2q2/b2p1n2/BP1Pp3/P1N2P2/2PB2PP/R2Q1RK1 w kq - 0 13",
+        // En-passent.
+        "rnbqkbnr/1pppp1pp/p7/4Pp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 1",
+        // Promotion.
+        "1r2k3/2P5/8/8/8/8/8/5K2 w - - 0 1",
+        // Castling.
+        "rnbqkbnr/pppppppp/8/8/8/8/P6P/R3K2R w KQkq - 0 1"
+    };
 
-    constexpr const char* EN_PASSENT_FEN { "rnbqkbnr/1pppp1pp/p7/4Pp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 1" };
-    std::cout << "en-passent\n";
-    std::cout << bitboard(EN_PASSENT_FEN);
-    std::cout << '\n';
-
-    constexpr const char* PROMOTION_FEN { "1r2k3/2P5/8/8/8/8/8/5K2 w - - 0 1" };
-    std::cout << "promotion\n";
-    std::cout << bitboard(PROMOTION_FEN);
-    std::cout << '\n';
-
-    std::cout << "First moves\n";
+    for (const auto& position : positions)
     {
-        bitboard start(PROMOTION_FEN);
-        for (auto move : generate_pseudo_legal_moves(start))
-        {
-            bitboard next_position = start;
-            make_move(next_position, move);
-            std::cout << next_position << '\n';
-        }
+        std::cout << "#####################################################################\n" << std::endl;;
+        std::cout << "fen - " << position << "\n" << std::endl;
+        display_pseudo_legal_moves_from_position(bitboard(position));
     }
 
     return EXIT_SUCCESS;
