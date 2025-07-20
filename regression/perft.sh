b9ceb72accd186c9bbc082efbef7ac49aae9dff8
Troy Sharples <troysharples@hotmail.co.uk>
2025-07-20 17:27:13 +0100

    Setup perft regression tests with tracking file

---
+++ regression/perft.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_PATH="${BASH_SOURCE:-$0}"
+SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
+
+# Runs the perft command - we always go from the starting position with depth 7.
+PERFT_PATH="${SCRIPT_DIR}"/../build/apps/perft
+PERFT_DEPTH=7
+PERFT_RESULT=$("${PERFT_PATH}" -y -d ${PERFT_DEPTH})
+
+# Adds additional metafields to the result and minifies the JSON.
+REGRESSION_TIMESTAMP=$(date +%s)
+REGRESSION_GIT=$(git rev-parse HEAD)
+REGRESSION_LABEL=$1
+REGRESSION_RESULT=$(echo ${PERFT_RESULT} | jq -c \
+    --arg regression_timestamp "${REGRESSION_TIMESTAMP}" \
+    --arg regression_git       "${REGRESSION_GIT}"       \
+    --arg regression_label     "${REGRESSION_LABEL}"     \
+    '. += {"timestamp": $regression_timestamp, "git": $regression_git, "label": $regression_label}')
+
+# Saves the result to the tracking json-lines file.
+REGRESSION_TRACKING_PATH="${SCRIPT_DIR}"/tracking/perft.ndjson
+echo ${REGRESSION_RESULT} >> "${REGRESSION_TRACKING_PATH}"
