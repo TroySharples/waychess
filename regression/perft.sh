1e3931b587eecd3f0bf07da1da60006599b8f308
Troy Sharples <troysharples@hotmail.co.uk>
2025-07-20 22:50:31 +0100

    Updated the perft regression script so that hardware type is also logged, and back-updated previous results

--- regression/perft.sh
+++ regression/perft.sh
@@ -11,14 +11,16 @@ PERFT_DEPTH=7
 PERFT_RESULT=$("${PERFT_PATH}" -y -d ${PERFT_DEPTH})
 
 # Adds additional metafields to the result and minifies the JSON.
+REGRESSION_HARDWARE=$(lscpu | grep 'Model name' | cut -f 2 -d ":" | awk '{$1=$1}1')
 REGRESSION_TIMESTAMP=$(date +%s)
 REGRESSION_GIT=$(git rev-parse HEAD)
 REGRESSION_LABEL=$1
 REGRESSION_RESULT=$(echo ${PERFT_RESULT} | jq -c \
+    --arg regression_hardware  "${REGRESSION_HARDWARE}" \
     --arg regression_timestamp "${REGRESSION_TIMESTAMP}" \
     --arg regression_git       "${REGRESSION_GIT}"       \
     --arg regression_label     "${REGRESSION_LABEL}"     \
-    '. += {"timestamp": $regression_timestamp, "git": $regression_git, "label": $regression_label}')
+    '. += {"hardware": $regression_hardware, "timestamp": $regression_timestamp, "git": $regression_git, "label": $regression_label}')
 
 # Saves the result to the tracking json-lines file.
 REGRESSION_TRACKING_PATH="${SCRIPT_DIR}"/tracking/perft.ndjson
