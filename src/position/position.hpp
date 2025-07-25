fd54a4291221543097918b1333026bf4e7579d3e
Troy Sharples <troysharples@hotmail.co.uk>
2025-07-25 23:21:10 +0100

    Added is-attacked functions that return bools. They seem to make things go slower though...

--- src/position/position.hpp
+++ src/position/position.hpp
@@ -37,4 +37,30 @@ inline std::uint64_t get_attackers_black(const bitboard& bb, auto... mb) noexcep
          | (get_rook_attacked_squares_from_mailboxes(pieces_bb, mb...)   & (bb.boards[piece_idx::b_rook]   | bb.boards[piece_idx::b_queen]));
 }
 
+inline bool is_attacked_white(const bitboard& bb, auto... mb) noexcept
+{
+    const std::uint64_t pieces_bb  { bb.boards[piece_idx::w_any] | bb.boards[piece_idx::b_any] };
+
+    if (get_black_pawn_all_attacked_squares_from_mailboxes(mb...)    & bb.boards[piece_idx::w_pawn])                                     [[unlikely]] return true;
+    if (get_king_attacked_squares_from_mailboxes(mb...)              & bb.boards[piece_idx::w_king])                                     [[unlikely]] return true;
+    if (get_knight_attacked_squares_from_mailboxes(mb...)            & bb.boards[piece_idx::w_knight])                                   [[unlikely]] return true;
+    if (get_bishop_attacked_squares_from_mailboxes(pieces_bb, mb...) & (bb.boards[piece_idx::w_bishop] | bb.boards[piece_idx::w_queen])) [[unlikely]] return true;
+    if (get_rook_attacked_squares_from_mailboxes(pieces_bb, mb...)   & (bb.boards[piece_idx::w_rook]   | bb.boards[piece_idx::w_queen])) [[unlikely]] return true;
+
+    return false;
+}
+
+inline bool is_attacked_black(const bitboard& bb, auto... mb) noexcept
+{
+    const std::uint64_t pieces_bb  { bb.boards[piece_idx::w_any] | bb.boards[piece_idx::b_any] };
+
+    if (get_white_pawn_all_attacked_squares_from_mailboxes(mb...)    & bb.boards[piece_idx::b_pawn])                                     [[unlikely]] return true;
+    if (get_king_attacked_squares_from_mailboxes(mb...)              & bb.boards[piece_idx::b_king])                                     [[unlikely]] return true;
+    if (get_knight_attacked_squares_from_mailboxes(mb...)            & bb.boards[piece_idx::b_knight])                                   [[unlikely]] return true;
+    if (get_bishop_attacked_squares_from_mailboxes(pieces_bb, mb...) & (bb.boards[piece_idx::b_bishop] | bb.boards[piece_idx::b_queen])) [[unlikely]] return true;
+    if (get_rook_attacked_squares_from_mailboxes(pieces_bb, mb...)   & (bb.boards[piece_idx::b_rook]   | bb.boards[piece_idx::b_queen])) [[unlikely]] return true;
+
+    return false;
+}
+
 
