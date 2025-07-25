bfbbc05b00a9153b3c2f847e60e39e8fd40036ae
Troy Sharples <troysharples@hotmail.co.uk>
2025-07-25 19:45:23 +0100

    Moved the is-attacked functions to the header so the compiler can inline them. This seems to have made a big difference (~5%)

--- src/position/position.hpp
+++ src/position/position.hpp
@@ -2,13 +2,39 @@
 
 #include "bitboard.hpp"
 
-// Returns a bitboard of all pieces (of any colour) attacking a particular square.
-std::uint64_t get_attackers(const bitboard& bb, std::uint8_t mb);
 
-// Returns a bitboard of all white pieces attacking a square.
-std::uint64_t get_attackers_white(const bitboard& bb, std::uint8_t mb);
+inline std::uint64_t get_attackers(const bitboard& bb, std::uint8_t mb) noexcept
+{
+    const std::uint64_t pieces_bb  { bb.boards[piece_idx::w_any] | bb.boards[piece_idx::b_any] };
 
-// Returns a bitboard of all black pieces attacking a square.
-std::uint64_t get_attackers_black(const bitboard& bb, std::uint8_t mb);
+    return (get_king_attacked_squares_from_mailbox(mb)              & (bb.boards[piece_idx::w_king]   | bb.boards[piece_idx::b_king]))
+         | (get_knight_attacked_squares_from_mailbox(mb)            & (bb.boards[piece_idx::w_knight] | bb.boards[piece_idx::b_knight]))
+         | (get_rook_attacked_squares_from_mailbox(mb, pieces_bb)   & (bb.boards[piece_idx::w_rook]   | bb.boards[piece_idx::b_rook]   | bb.boards[piece_idx::w_queen] | bb.boards[piece_idx::b_queen]))
+         | (get_bishop_attacked_squares_from_mailbox(mb, pieces_bb) & (bb.boards[piece_idx::w_bishop] | bb.boards[piece_idx::b_bishop] | bb.boards[piece_idx::w_queen] | bb.boards[piece_idx::b_queen]))
+         | (get_white_pawn_all_attacked_squares_from_mailbox(mb)    & bb.boards[piece_idx::b_pawn])
+         | (get_black_pawn_all_attacked_squares_from_mailbox(mb)    & bb.boards[piece_idx::w_pawn]);
+}
+
+inline std::uint64_t get_attackers_white(const bitboard& bb, std::uint8_t mb) noexcept
+{
+    const std::uint64_t pieces_bb  { bb.boards[piece_idx::w_any] | bb.boards[piece_idx::b_any] };
+
+    return (get_black_pawn_all_attacked_squares_from_mailbox(mb)    & bb.boards[piece_idx::w_pawn])
+         | (get_king_attacked_squares_from_mailbox(mb)              & bb.boards[piece_idx::w_king])
+         | (get_knight_attacked_squares_from_mailbox(mb)            & bb.boards[piece_idx::w_knight])
+         | (get_bishop_attacked_squares_from_mailbox(mb, pieces_bb) & (bb.boards[piece_idx::w_bishop] | bb.boards[piece_idx::w_queen]))
+         | (get_rook_attacked_squares_from_mailbox(mb, pieces_bb)   & (bb.boards[piece_idx::w_rook]   | bb.boards[piece_idx::w_queen]));
+}
+
+inline std::uint64_t get_attackers_black(const bitboard& bb, std::uint8_t mb) noexcept
+{
+    const std::uint64_t pieces_bb  { bb.boards[piece_idx::w_any] | bb.boards[piece_idx::b_any] };
+
+    return (get_white_pawn_all_attacked_squares_from_mailbox(mb)    & bb.boards[piece_idx::b_pawn])
+         | (get_king_attacked_squares_from_mailbox(mb)              & bb.boards[piece_idx::b_king])
+         | (get_knight_attacked_squares_from_mailbox(mb)            & bb.boards[piece_idx::b_knight])
+         | (get_bishop_attacked_squares_from_mailbox(mb, pieces_bb) & (bb.boards[piece_idx::b_bishop] | bb.boards[piece_idx::b_queen]))
+         | (get_rook_attacked_squares_from_mailbox(mb, pieces_bb)   & (bb.boards[piece_idx::b_rook]   | bb.boards[piece_idx::b_queen]));
+}
 
 
