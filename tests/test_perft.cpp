44fda9aae422e034f45ad19468036bdbe91a9d32
Troy Sharples <troysharples@hotmail.co.uk>
2025-07-26 16:03:32 +0100

    Added gtest unmake tests

--- tests/test_perft.cpp
+++ tests/test_perft.cpp
@@ -1,5 +1,7 @@
 #include "utility/perft.hpp"
 
+#include "test_positions.hpp"
+
 #include <gtest/gtest.h>
 
 /////////////////////////////////////////////////////////////////////////
@@ -9,7 +11,6 @@
 
 TEST(Perft, StartingPosition)
 {
-    constexpr const char* fen { "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" };
     std::vector<std::size_t> results {
         1,
         20,
@@ -22,12 +23,11 @@ TEST(Perft, StartingPosition)
     };
 
     for (std::size_t i = 0; i < results.size(); i++)
-        EXPECT_EQ(perft(bitboard(fen), i), results[i]);
+        EXPECT_EQ(perft(bitboard(STARTING_FEN), i), results[i]);
 }
 
 TEST(Perft, Kiwipete)
 {
-    constexpr const char* fen { "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1" };
     std::vector<std::size_t> results {
         1,
         48,
@@ -39,12 +39,11 @@ TEST(Perft, Kiwipete)
     };
 
     for (std::size_t i = 0; i < results.size(); i++)
-        EXPECT_EQ(perft(bitboard(fen), i), results[i]);
+        EXPECT_EQ(perft(bitboard(KIWIPETE_FEN), i), results[i]);
 }
 
 TEST(Perft, Pos3)
 {
-    constexpr const char* fen { "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1" };
     std::vector<std::size_t> results {
         1,
         14,
@@ -58,12 +57,11 @@ TEST(Perft, Pos3)
     };
 
     for (std::size_t i = 0; i < results.size(); i++)
-        EXPECT_EQ(perft(bitboard(fen), i), results[i]);
+        EXPECT_EQ(perft(bitboard(POS3_FEN), i), results[i]);
 }
 
 TEST(Perft, Pos4)
 {
-    constexpr const char* fen { "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1" };
     std::vector<std::size_t> results {
         1,
         6,
@@ -75,12 +73,11 @@ TEST(Perft, Pos4)
     };
 
     for (std::size_t i = 0; i < results.size(); i++)
-        EXPECT_EQ(perft(bitboard(fen), i), results[i]);
+        EXPECT_EQ(perft(bitboard(POS4_FEN), i), results[i]);
 }
 
 TEST(Perft, Pos5)
 {
-    constexpr const char* fen { "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8" };
     std::vector<std::size_t> results {
         1,
         44,
@@ -91,12 +88,11 @@ TEST(Perft, Pos5)
     };
 
     for (std::size_t i = 0; i < results.size(); i++)
-        EXPECT_EQ(perft(bitboard(fen), i), results[i]);
+        EXPECT_EQ(perft(bitboard(POS5_FEN), i), results[i]);
 }
 
 TEST(Perft, Pos6)
 {
-    constexpr const char* fen { "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10" };
     std::vector<std::size_t> results {
         1,
         46,
@@ -107,7 +103,7 @@ TEST(Perft, Pos6)
     };
 
     for (std::size_t i = 0; i < results.size(); i++)
-        EXPECT_EQ(perft(bitboard(fen), i), results[i]);
+        EXPECT_EQ(perft(bitboard(POS6_FEN), i), results[i]);
 }
 
 int main(int argc, char **argv)
---
+++ tests/test_unmake.cpp
@@ -0,0 +1,80 @@
+#include "position/generate_moves.hpp"
+#include "position/make_move.hpp"
+#include "position/move.hpp"
+
+#include "test_positions.hpp"
+
+#include <gtest/gtest.h>
+
+namespace
+{
+
+void test_unmake_recursive(bitboard& bb, std::size_t depth, std::span<std::uint32_t> move_buf)
+{
+    if (depth == 0)
+        return;
+
+    const std::size_t moves { generate_pseudo_legal_moves(bb, move_buf) };
+
+    const bitboard orig { bb };
+    for (std::size_t i = 0; i < moves; i++)
+    {
+        constexpr make_move_args args { .check_legality = false };
+
+        std::uint32_t unmake {};
+        make_move(args, bb, move_buf[i], unmake);
+
+        test_unmake_recursive(bb, depth-1, move_buf.subspan(moves));
+
+        unmake_move(bb, unmake);
+
+        EXPECT_EQ(bb, orig);
+    }
+}
+
+void test_unmake(const char* fen, std::size_t depth)
+{
+    constexpr std::size_t max_moves_per_position { 218 };
+    std::vector<std::uint32_t> move_buf(depth*max_moves_per_position);
+
+    bitboard bb { fen };
+    test_unmake_recursive(bb, depth, move_buf);
+}
+
+}
+
+TEST(Unmake, StartingPosition)
+{
+    test_unmake(STARTING_FEN, 7);
+}
+
+TEST(Unmake, Kiwipete)
+{
+    test_unmake(KIWIPETE_FEN, 6);
+}
+
+TEST(Unmake, Pos3)
+{
+    test_unmake(POS3_FEN, 8);
+}
+
+TEST(Unmake, Pos4)
+{
+    test_unmake(POS4_FEN, 6);
+}
+
+TEST(Unmake, Pos5)
+{
+    test_unmake(POS5_FEN, 5);
+}
+
+TEST(Unmake, Pos6)
+{
+    test_unmake(POS6_FEN, 5);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
