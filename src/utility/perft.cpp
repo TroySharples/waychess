c0038b9ffd584954ce6d44645493c5cbc89cd3d7
Troy Sharples <troysharples@hotmail.co.uk>
2025-07-26 02:11:34 +0100

    Small change to perft (no performance difference)

--- src/utility/perft.cpp
+++ src/utility/perft.cpp
@@ -1,3 +1,5 @@
+#include "perft.hpp"
+
 #include "position/generate_moves.hpp"
 #include "position/make_move.hpp"
 #include "position/move.hpp"
@@ -7,11 +9,11 @@ namespace
 
 std::size_t perft_recursive(const bitboard& bb, std::size_t depth, std::span<std::uint32_t> move_buf)
 {
-    if (depth == 0)
+    if (depth == 0) [[unlikely]]
         return 1;
 
     std::size_t ret {};
-    
+
     const std::size_t moves { generate_pseudo_legal_moves(bb, move_buf) };
 
     for (std::size_t i = 0; i < moves; i++)
