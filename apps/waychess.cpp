8729b89c7b6fa7f214ef7d2c624f791fc6566efe
Troy Sharples <troysharples@hotmail.co.uk>
2025-07-29 20:19:10 +0100

    Initial commit of barebones UCI interface skeleton. Will continue this on main PC as it has a desktop

--- apps/waychess.cpp
+++ apps/waychess.cpp
@@ -1,103 +1,85 @@
+#include "position/bitboard.hpp"
+
 #include <cstdlib>
+#include <iostream>
+#include <string>
+#include <sstream>
 
-#include "position/generate_moves.hpp"
-#include "position/make_move.hpp"
-#include "position/move.hpp"
-
-static void display_pseudo_legal_moves_from_position(const bitboard& start)
+namespace
 {
-    std::cout << "starting-position:" << std::endl;
-    start.display_unicode_board(std::cout) << std::endl;
 
-    constexpr make_move_args args { .check_legality = false };
+// Our global bitboard - we'll think about wrapping this in a nice class at some point.
+bitboard bb;
 
-    std::array<std::uint32_t, 256> move_buf;
-    const std::size_t moves { generate_pseudo_legal_moves(start, move_buf) };
-    for (std::size_t i = 0; i < moves; i++)
-    {
-        bitboard next_position = start;
-        make_move(args, next_position, move_buf[i]);
-
-        std::cout << "move: " << move::to_algebraic_long(move_buf[i]) << ":" << std::endl;
-        next_position.display_unicode_board(std::cout) << std::endl;
-    }
+void handle_uci()
+{
+    std::cout << "id name Waychess\n"
+              << "id author Troy Sharples\n"
+              << "uciok" << std::endl;
 }
 
-static void display_strictly_legal_moves_from_position(const bitboard& start)
+void handle_isready()
 {
-    std::cout << "starting-position:" << std::endl;
-    start.display_unicode_board(std::cout) << std::endl;
+    std::cout << "readyok" << std::endl;
+}
 
-    constexpr make_move_args args { .check_legality = true };
+void handle_ucinewgame()
+{
+    
+}
 
-    std::array<std::uint32_t, 256> move_buf;
-    const std::size_t moves { generate_pseudo_legal_moves(start, move_buf) };
-    for (std::size_t i = 0; i < moves; i++)
-    {
-        bitboard next_position = start;
-        if (!make_move(args, next_position, move_buf[i]))
-            continue;
+void handle_debug(std::istringstream& /*iss*/)
+{
+    std::cerr << "We do not support the debug command" << std::endl;
+}
 
-        std::cout << "move: " << move::to_algebraic_long(move_buf[i]) << ":" << std::endl;
-        next_position.display_unicode_board(std::cout);
-        std::cout << next_position.get_fen_string() << '\n' << std::endl;
-    }
+void handle_position(std::istringstream& iss)
+{
+    std::string fen;
+    std::getline(iss >> std::ws, fen);
+
+    bitboard bb(fen);
+    bb.display_unicode_board(std::cout) << std::endl;
+}
+
+void handle_go(std::istringstream& iss)
+{
+    
+}
+
+void handle_unknown(std::string_view command)
+{
+    std::cerr << "Unknown command - " << command << std::endl;
+}
+
 }
 
 int main()
 {
+    std::string line;
+
+    while (std::getline(std::cin, line))
     {
-        std::cout << "pseudo-legal move-generation:\n\n";
+        std::istringstream iss(line);
+        std::string command;
+        iss >> command;
 
-        std::vector<std::string> positions {
-            // Starting position.
-            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-            // Complicated middlegame.
-            "r3k2r/pp1n2pp/2p2q2/b2p1n2/BP1Pp3/P1N2P2/2PB2PP/R2Q1RK1 w kq - 0 13",
-            // En-passent.
-            "rnbqkbnr/1pppp1pp/p7/4Pp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 1",
-            // Promotion.
-            "1r2k3/2P5/8/8/8/8/8/5K2 w - - 0 1",
-            // Castling.
-            "rnbqkbnr/pppppppp/8/8/8/8/P6P/R3K2R w KQkq - 0 1"
-        };
-
-        for (const auto& position : positions)
-        {
-            std::cout << "#####################################################################\n" << std::endl;
-            std::cout << "fen - " << position << "\n" << std::endl;
-            display_pseudo_legal_moves_from_position(bitboard(position));
-        }
-    }
-
-    {
-        std::cout << "strictly-legal move-generation:\n\n";
-
-        std::vector<std::string> positions {
-            // Starting position.
-            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-            // Checks everywhere.
-            "8/2K5/R7/4k3/6B1/2N5/8/8 b - - 0 1",
-            // Castling from check.
-            "r3k2r/p6p/8/3K3B/8/8/8/8 b kq - 0 1",
-            // Castling into check.
-            "r3k2r/p6p/8/3K4/6B1/8/8/8 b kq - 0 1",
-            // Castling through check king-side.
-            "r3k2r/p6p/8/2BK4/8/8/8/8 b kq - 0 1",
-            // Castling through check queen-side.
-            "r3k2r/p6p/5B2/3K4/8/8/8/8 b kq - 0 1",
-            // Talkchess gotcha.
-            "r3k2r/Pppp1ppp/1b3nbN/nPB5/B1P1P3/q4N2/Pp1P2PP/R2Q1RK1 b kq - 1 1",
-            // Kiwipete gotcha.
-            "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/2KR3R b kq - 1 1"
-        };
-
-        for (const auto& position : positions)
-        {
-            std::cout << "#####################################################################\n" << std::endl;
-            std::cout << "fen - " << position << "\n" << std::endl;
-            display_strictly_legal_moves_from_position(bitboard(position));
-        }
+        if (command == "uci")
+            handle_uci();
+        else if (command == "isready")
+            handle_isready();
+        else if (command == "ucinewgame")
+            handle_ucinewgame();
+        else if (command == "debug")
+            handle_debug(iss);
+        else if (command == "position")
+            handle_position(iss);
+        else if (command == "go")
+            handle_go(iss);
+        else if (command == "quit")
+            return EXIT_SUCCESS;
+        else
+            handle_unknown(command);
     }
 
     return EXIT_SUCCESS;
