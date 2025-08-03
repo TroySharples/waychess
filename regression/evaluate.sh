2b3f04fc024423f51fcf8647fb52366aae6c08a4
Troy Sharples <troysharples@hotmail.co.uk>
2025-08-03 01:22:40 +0100

    Added evaluation regression test

---
+++ regression/evaluate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_PATH="${BASH_SOURCE:-$0}"
+SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
+
+# Runs the evaluate command - we always go from the starting position with depth 7.
+EVALUATE_PATH="${SCRIPT_DIR}"/../build/apps/waychess-evaluate
+EVALUATE_DEPTH=7
+EVALUATE_RESULT=$("${EVALUATE_PATH}" -y -d ${EVALUATE_DEPTH})
+
+# Adds additional metafields to the result and minifies the JSON.
+REGRESSION_HARDWARE=$(lscpu | grep 'Model name' | cut -f 2 -d ":" | awk '{$1=$1}1')
+REGRESSION_TIMESTAMP=$(date +%s)
+REGRESSION_GIT=$(git rev-parse HEAD)
+REGRESSION_LABEL=$1
+REGRESSION_RESULT=$(echo ${EVALUATE_RESULT} | jq -c \
+    --arg regression_hardware  "${REGRESSION_HARDWARE}" \
+    --arg regression_timestamp "${REGRESSION_TIMESTAMP}" \
+    --arg regression_git       "${REGRESSION_GIT}"       \
+    --arg regression_label     "${REGRESSION_LABEL}"     \
+    '. += {"hardware": $regression_hardware, "timestamp": $regression_timestamp, "git": $regression_git, "label": $regression_label}')
+
+# Saves the result to the tracking json-lines file.
+REGRESSION_TRACKING_PATH="${SCRIPT_DIR}"/tracking/evaluate.ndjson
+echo ${REGRESSION_RESULT} >> "${REGRESSION_TRACKING_PATH}"
