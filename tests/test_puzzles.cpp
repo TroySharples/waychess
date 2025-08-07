a8a0bbee789f91ce731691f8503d28a121b8d2eb
Troy Sharples <troysharples@hotmail.co.uk>
2025-08-08 00:34:10 +0100

    Added the framework for multiple puzzle-solving formats (e.g. end-games, mates, etc)

--- tests/test_puzzles.cpp
+++ tests/test_puzzles.cpp
@@ -2,31 +2,31 @@
 #include "position/search.hpp"
 
 #include <gtest/gtest.h>
+#include <filesystem>
 #include <fstream>
 
-TEST(Puzzles, Mate5000)
+namespace
+{
+
+void test_puzzles(const std::filesystem::path& csv_path, double pass_accuracy)
 {
     std::size_t puzzles_total {};
     std::size_t puzzles_solved {};
 
-    std::ifstream is(PUZZLE_CSV_PATH);
-    if (!is)
-        throw std::runtime_error("Unable to open puzzle CSV file");
+    std::ifstream is(csv_path);
+    ASSERT_TRUE(is);
 
-    // Absorb the one-line header of column labels.
-    std::string token;
-    if (!getline(is, token))
-        throw std::runtime_error("Unable to parse CSV column header");
+    std::string line;
+    ASSERT_TRUE(std::getline(is, line));
 
-    const recommendation rec = [] (const bitboard& bb) { 
+    const recommendation rec = [] (const bitboard& bb) {
         constexpr std::uint8_t depth { 4 };
-
         return best_move_minimax(bb, depth, &evaluate_terminal).first;
     };
 
-    while (getline(is, token))
+    while (std::getline(is, line))
     {
-        std::stringstream ss(token);
+        std::stringstream ss(line);
 
         puzzle p;
         ss >> p;
@@ -35,16 +35,25 @@ TEST(Puzzles, Mate5000)
         if (p.solve(rec)) puzzles_solved++;
     }
 
-    const double accuracy = static_cast<double>(puzzles_solved)/static_cast<double>(puzzles_total);
+    const double accuracy = static_cast<double>(puzzles_solved) / static_cast<double>(puzzles_total);
 
-    std::cout << "puzzles total  - " << puzzles_total << '\n';
-    std::cout << "puzzles solved - " << puzzles_solved << '\n';
-    std::cout << "accuracy       - " << accuracy*100 << '\n';
-
-    EXPECT_GE(accuracy, 0.6);
+    EXPECT_GE(accuracy, pass_accuracy);
 }
 
-// Tests performannt perft correctness across a variety of positions.
+}
+
+TEST(Puzzle, Mate500)
+{
+    test_puzzles(std::filesystem::path(PUZZLE_DIR) / "mate500.csv", 0.9);
+}
+
+TEST(Puzzle, Endgame500)
+{
+    test_puzzles(std::filesystem::path(PUZZLE_DIR) / "endgame500.csv", 0.5);
+}
+
+// Tests the rough performance of the search and evaluation by evaluate puzzle-solving
+// performance across a variety of position types.
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
