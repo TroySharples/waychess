ea47bd4b04d07c7c1e022063d1afbf37550bb76b
Troy Sharples <troysharples@hotmail.co.uk>
2025-08-08 15:56:57 +0100

    Added regression script for puzzle solving

---
+++ regression/puzzler.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_PATH="${BASH_SOURCE:-$0}"
+SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
+
+# Runs the command.
+PUZZLER_PATH="${SCRIPT_DIR}"/../build/apps/waychess-puzzler
+PUZZLER_CSV=$1
+PUZZLER_DEPTH=$2
+PUZZLER_RESULT=$("${PUZZLER_PATH}" -f "${PUZZLER_CSV}" -d ${PUZZLER_DEPTH})
+
+# Adds additional metafields to the result and minifies the JSON.
+REGRESSION_HARDWARE=$(lscpu | grep 'Model name' | cut -f 2 -d ":" | awk '{$1=$1}1')
+REGRESSION_TIMESTAMP=$(date +%s)
+REGRESSION_GIT=$(git rev-parse HEAD)
+REGRESSION_LABEL=$3
+REGRESSION_RESULT=$(echo ${PUZZLER_RESULT} | jq -c \
+    --arg regression_hardware  "${REGRESSION_HARDWARE}" \
+    --arg regression_timestamp "${REGRESSION_TIMESTAMP}" \
+    --arg regression_git       "${REGRESSION_GIT}"       \
+    --arg regression_label     "${REGRESSION_LABEL}"     \
+    '. += {"hardware": $regression_hardware, "timestamp": $regression_timestamp, "git": $regression_git, "label": $regression_label}')
+
+# Saves the result to the tracking json-lines file.
+REGRESSION_TRACKING_PATH="${SCRIPT_DIR}"/tracking/puzzler.ndjson
+echo ${REGRESSION_RESULT} >> "${REGRESSION_TRACKING_PATH}"
