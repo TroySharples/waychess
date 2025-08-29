d134c3b4ea9391e2a4abf5a7073a5fed29d87759
Troy Sharples <troysharples@hotmail.co.uk>
2025-08-29 20:11:25 +0100

    Updated run-suite to remove the old strategy and search options

--- regression/run_suite.sh
+++ regression/run_suite.sh
@@ -20,19 +20,14 @@ run() {
 
 RUN_PERFT_PATH="${SCRIPT_DIR}"/perft.sh
 
-# Basic depth-7 perft across all strategies and various hash-sizes.
-for strategy in copy unmake; do
-    for hash_mb in 0 20 100 500 2000; do
-        run "${RUN_PERFT_PATH}" "${strategy}" 7 "${hash_mb}" "${SUITE_LABEL}"
-    done
+# Basic depth-7 perft across various hash-sizes.
+for hash_mb in 1 2 5 10 20 50 100 200 500 1000 2000 4000; do
+    run "${RUN_PERFT_PATH}" 7 "${hash_mb}" "${SUITE_LABEL}"
 done
 
-# Deep depth-8 perft with fewer hash-sizes. We omit the case of no hash table
-# as it just takes too long...
-for strategy in copy unmake; do
-    for hash_mb in 50 500 4000; do
-        run "${RUN_PERFT_PATH}" "${strategy}" 8 "${hash_mb}" "${SUITE_LABEL}"
-    done
+# Deep depth-8 perft with fewer hash-sizes.
+for hash_mb in 50 500 4000; do
+    run "${RUN_PERFT_PATH}" 8 "${hash_mb}" "${SUITE_LABEL}"
 done
 
 # ###############################################################################
@@ -41,17 +36,10 @@ done
 
 RUN_EVALUATE_PATH="${SCRIPT_DIR}"/evaluate.sh
 
-# Basic depth-6 evaluate (going deeper would take too long for non alpha/beta)
-# across all searches. We use no hash here, as only negamax alpha/beta implements
-# it.
-for search in minimax negamax negamax-prune; do
-    run "${RUN_EVALUATE_PATH}" "${search}" 6 0 "${SUITE_LABEL}"
-done
-
-# Focus on alpha/beta and go deeper across a variety of hash-sizes.
+# Basic depths 7 and 8 evaluate across various hash-sizes.
 for depth in 7 8; do
-    for hash_mb in 0 50 500 4000; do
-        run "${RUN_EVALUATE_PATH}" "negamax-prune" "${depth}" "${hash_mb}" "${SUITE_LABEL}"
+    for hash_mb in 1 5 50 200 1000 4000; do
+        run "${RUN_EVALUATE_PATH}" "${depth}" "${hash_mb}" "${SUITE_LABEL}"
     done
 done
 
@@ -62,20 +50,11 @@ done
 RUN_PUZZLER_PATH="${SCRIPT_DIR}"/puzzler.sh
 PUZZLE_DIR="${SCRIPT_DIR}"/../puzzles
 
-# Basic depth-4 solve (going deeper would take too long for non alpha/beta)
-# across all searches and all puzzle files. We use no hash here, as only negamax
-# alpha/beta implements it.
-for file in mate500 endgame500; do
-    for search in minimax negamax negamax-prune; do
-        run "${RUN_PUZZLER_PATH}" "${PUZZLE_DIR}/${file}.csv" "${search}" 4 0 "${SUITE_LABEL}"
-    done
-done
-
-# Focus on alpha/beta and go deeper across a variety of hash-sizes.
+# Basic depths 4 and 5 solve across all puzzle files and a variety of hash-sizes.
 for file in mate500 endgame500; do
     for depth in 4 5; do
-        for hash_mb in 0 50 500 4000; do
-            run "${RUN_PUZZLER_PATH}" "${PUZZLE_DIR}/${file}.csv" "negamax-prune" "${depth}" "${hash_mb}" "${SUITE_LABEL}"
+        for hash_mb in 1 5 50 200 1000 4000; do
+            run "${RUN_PUZZLER_PATH}" "${PUZZLE_DIR}/${file}.csv" "${depth}" "${hash_mb}" "${SUITE_LABEL}"
         done
     done
 done
