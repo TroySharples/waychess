fc0fabb8cf549f220135c7c1f8570257af367b4f
Troy Sharples <troysharples@hotmail.co.uk>
2025-09-21 21:48:07 +0100

    Revised pawn penalty after observing bad SPRT performance - this is about as good as (maybe slightly better) than not doing any structural weighting

--- src/evaluation/evaluate_pawn_structure.hpp
+++ src/evaluation/evaluate_pawn_structure.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "pieces/pawn.hpp"
 #include "pieces/pieces.hpp"
 #include "position/bitboard.hpp"
 
@@ -10,14 +9,14 @@ namespace evaluation
 namespace details
 {
 
-constexpr int isolated_pawns_penalty_mg { 27 };
-constexpr int isolated_pawns_penalty_eg { 54 };
+constexpr int isolated_pawns_penalty_mg { 10 };
+constexpr int isolated_pawns_penalty_eg { 15 };
 
-constexpr int doubled_pawns_penalty_mg { 15 };
-constexpr int doubled_pawns_penalty_eg { 55 };
+constexpr int doubled_pawns_penalty_mg { 8 };
+constexpr int doubled_pawns_penalty_eg { 15 };
 
-constexpr int blocked_pawns_penalty_mg { 24 };
-constexpr int blocked_pawns_penalty_eg { 34 };
+constexpr int blocked_pawns_penalty_mg { 12 };
+constexpr int blocked_pawns_penalty_eg { 10 };
 
 }
 
@@ -27,10 +26,10 @@ inline int evaluate_pawn_structure_mg(const bitboard& bb)
 
     return std::popcount(get_pawn_isolated(bb.boards[b_pawn]))           * details::isolated_pawns_penalty_mg
          + std::popcount(get_pawn_doubled(bb.boards[b_pawn]))            * details::doubled_pawns_penalty_mg
-         + std::popcount(get_black_pawn_blocked(bb.boards[b_pawn], occ)) * details::doubled_pawns_penalty_mg
+         + std::popcount(get_black_pawn_blocked(bb.boards[b_pawn], occ)) * details::blocked_pawns_penalty_mg
          - std::popcount(get_pawn_isolated(bb.boards[w_pawn]))           * details::isolated_pawns_penalty_mg
          - std::popcount(get_pawn_doubled(bb.boards[w_pawn]))            * details::doubled_pawns_penalty_mg
-         - std::popcount(get_white_pawn_blocked(bb.boards[w_pawn], occ)) * details::doubled_pawns_penalty_mg;
+         - std::popcount(get_white_pawn_blocked(bb.boards[w_pawn], occ)) * details::blocked_pawns_penalty_mg;
 }
 
 inline int evaluate_pawn_structure_eg(const bitboard& bb)
@@ -39,10 +38,10 @@ inline int evaluate_pawn_structure_eg(const bitboard& bb)
 
     return std::popcount(get_pawn_isolated(bb.boards[b_pawn]))           * details::isolated_pawns_penalty_eg
          + std::popcount(get_pawn_doubled(bb.boards[b_pawn]))            * details::doubled_pawns_penalty_eg
-         + std::popcount(get_black_pawn_blocked(bb.boards[b_pawn], occ)) * details::doubled_pawns_penalty_eg
+         + std::popcount(get_black_pawn_blocked(bb.boards[b_pawn], occ)) * details::blocked_pawns_penalty_eg
          - std::popcount(get_pawn_isolated(bb.boards[w_pawn]))           * details::isolated_pawns_penalty_eg
          - std::popcount(get_pawn_doubled(bb.boards[w_pawn]))            * details::doubled_pawns_penalty_eg
-         - std::popcount(get_white_pawn_blocked(bb.boards[w_pawn], occ)) * details::doubled_pawns_penalty_eg;
+         - std::popcount(get_white_pawn_blocked(bb.boards[w_pawn], occ)) * details::blocked_pawns_penalty_eg;
 }
 
 }
